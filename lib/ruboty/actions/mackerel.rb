b7e3d54e9f945920d638d6edf48f9935a6a93e2d
mizokami <r.mizokami@gmail.com>
2015-11-09 08:12:14 +0900

    Remove unnessesory module definition

--- lib/ruboty/actions/mackerel.rb
+++ lib/ruboty/actions/mackerel.rb
@@ -4,10 +4,3 @@ require "ruboty/actions/mackerel/list_services"
 require "ruboty/actions/mackerel/list_service_roles"
 require "ruboty/actions/mackerel/list_service_role_hosts"
 require "ruboty/actions/mackerel/list_service_hosts"
-
-module Ruboty
-  module Actions
-    module Mackerel
-    end
-  end
-end
