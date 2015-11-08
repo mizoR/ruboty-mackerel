87b50b02b8bb1afe142d5549d3132a260cb0a1cf
mizokami <r.mizokami@gmail.com>
2015-11-09 08:36:49 +0900

    Simplify regexp pattern

--- lib/ruboty/handlers/mackerel.rb
+++ lib/ruboty/handlers/mackerel.rb
@@ -8,7 +8,7 @@ module Ruboty
       on %r<mkr services? (\w+) roles?$>,              name: 'list_service_roles',      description: 'List service roles.'
       on %r<mkr services? (\w+) roles? (\w+)? hosts$>, name: 'list_service_role_hosts', description: 'List service role hosts.'
       on %r<mkr services? (\w+) hosts?$>,              name: 'list_service_hosts',      description: 'List service hosts.'
-      on %r<mkr( (.+))?>,                              name: 'help',                    description: 'Help.', missing: true, hidden: true
+      on %r<mkr(.+)?>,                                 name: 'help',                    description: 'Help.', missing: true, hidden: true
 
       def list_services(message)
         Ruboty::Actions::Mackerel::ListServices.new(message).call
