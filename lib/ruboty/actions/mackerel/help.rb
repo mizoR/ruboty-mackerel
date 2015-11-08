7e7c3e230a7f1e1433873f4b240625e0c48ecf37
mizokami <r.mizokami@gmail.com>
2015-11-09 08:28:51 +0900

    Rich help

--- lib/ruboty/actions/mackerel/help.rb
+++ lib/ruboty/actions/mackerel/help.rb
@@ -6,15 +6,16 @@ module Ruboty
 
         def get_body
           descriptions = get_all_descriptions
-          descriptions.select! { |description| description.include?('mkr') }
-          descriptions.join("\n")
+          descriptions.select! { |pattern, _| pattern.include?('mkr') }
+          "Command not found:\n    #{message.body}\n\nHelp:\n" +
+            Terminal::Table.new(rows: descriptions).to_s
         end
 
         def get_all_descriptions
           Ruboty.actions.reject(&:hidden?).sort.map do |action|
             prefix = ""
             prefix << message.robot.name << " " unless  action.all?
-            "#{prefix}#{action.pattern.inspect} - #{action.description}"
+            ["#{prefix}#{action.pattern.inspect}", action.description]
           end
         end
       end
